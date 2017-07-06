1c343b8b341c5d47c2d8c17655759799dc698e8d
bscheshirwork <bscheshir.work@gmail.com>
2017-07-06 18:29:19 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace machour\yii2\google\apiclient;
+namespace bscheshirwork\yii2\google\apiclient;
 
 use yii\base\BootstrapInterface;
 use yii\base\Module as BaseModule;
 
 class Module extends BaseModule implements BootstrapInterface
 {
-    public $controllerNamespace = 'machour\yii2\google\apiclient\controllers';
+    public $controllerNamespace = 'bscheshirwork\yii2\google\apiclient\controllers';
 
     public function bootstrap($app)
     {
         if ($app instanceof \yii\console\Application) {
             $app->controllerMap[$this->id] = [
-                'class' => 'machour\yii2\google\apiclient\commands\GoogleController',
+                'class' => 'bscheshirwork\yii2\google\apiclient\commands\GoogleController',
                 'module' => $this,
             ];
         }
